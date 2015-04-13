43440a7cd50bcc6799f2809e4c1fee39f8a03b57
ecnewbie <ecnewbie@gmail.com>
2015-04-13 22:29:29 +0800

    always use zenburn theme.

--- lisp/init-looks.el
+++ lisp/init-looks.el
@@ -1,8 +1,7 @@
 (require 'init-gui-frames)				;; setting for frame. from purcell.
 (require 'init-frame-hooks)				;; frame hooks, for add hook after make frame. from purcell.
 (require 'init-xterm)					;; setting for virtual terminal. from redguardtoo.
-(require 'init-themes)					;; dark and light two themes. from purcell.
-(require 'init-my-themes)
+(require 'init-themes)					;; use zenburn now.
 (require 'init-windows)					;; setting for switch window, winner and split window... from purcell.
 
 (require-package 'window-numbering)	
