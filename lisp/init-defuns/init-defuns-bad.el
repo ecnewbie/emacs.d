37ad7d29de0cd85cd5883d7bed7a037dfe5dad5e
ecnewbie <ecnewbie@gmail.com>
2015-10-15 23:43:23 +0800

    fix toggle-comment-c++-word.

--- lisp/init-defuns/init-defuns-bad.el
+++ lisp/init-defuns/init-defuns-bad.el
@@ -1,13 +1,13 @@
 (defun search-cpp-reference()
   "search http://en.cppreference.com."
   (interactive)
-   (progn
-     (let* ((key-word))
-       (setq key-word (read-from-minibuffer (concat "search " (current-word) "?")))
-       (when (equal key-word "")
-         (setq key-word (current-word)))
-       (eww-browse-url
-        (concat "http://en.cppreference.com/mwiki/index.php?search=" key-word)))))
+  (progn
+    (let* ((key-word))
+      (setq key-word (read-from-minibuffer (concat "search " (current-word) "?")))
+      (when (equal key-word "")
+        (setq key-word (current-word)))
+      (eww-browse-url
+       (concat "http://en.cppreference.com/mwiki/index.php?search=" key-word)))))
 
 (defun man2()
   "man 2 [key-word]."
@@ -22,18 +22,25 @@
 (defun toggle-comment-c++-word()
   "use /* word */ style to comment current word"
   (interactive)
-  (let* ((start) (end))
-        (skip-syntax-backward "^ \t,()")
-        (setq start (point))
-        (skip-syntax-forward "^ \t,()")
-        (setq end (point))
-        (unless (= start end)
-          (let* (word)
-            (setq word (buffer-substring-no-properties start end))
-            (delete-region start end)
-            (if (string-match "/\\*.*\\*/" word)
-                (insert (substring word 2 -2))
-              (insert (concat "/*" word "*/")))))))
+  (let* ((start) (pos (point)) (end))
+    (beginning-of-line)
+    (setq start (point))
+    (end-of-line)
+    (setq end (point))
+    (goto-char pos)
+
+    (skip-chars-backward "^ ,;()\t" start)
+    (setq start (point))
+    (skip-chars-forward "^ ,;()\t" end)
+    (setq end (point))
+
+    (unless (= start end)
+      (let* (word)
+        (setq word (buffer-substring-no-properties start end))
+        (delete-region start end)
+        (if (string-match "/\\*.*\\*/" word)
+            (insert (substring word 2 -2))
+          (insert (concat "/*" word "*/")))))))
 
 (defun replace-string-from-to (from to)
   (goto-char 0)
