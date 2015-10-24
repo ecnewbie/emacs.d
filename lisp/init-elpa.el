e1fba5caae2aef21e7ed2e194d0fb1b927d1c4eb
ecnewbie <ecnewbie@gmail.com>
2015-10-24 15:37:22 +0800

    do not install some package from melpa.

--- lisp/init-elpa.el
+++ lisp/init-elpa.el
@@ -1,53 +1,46 @@
-;;; Find and load the correct package.el
-
-;; When switching between Emacs 23 and 24, we always use the bundled package.el in Emacs 24
-(let ((package-el-site-lisp-dir
-       (expand-file-name "site-lisp/package" user-emacs-directory)))
-  (when (and (file-directory-p package-el-site-lisp-dir)
-             (> emacs-major-version 23))
-    (message "Removing local package.el from load-path to avoid shadowing bundled version")
-    (setq load-path (remove package-el-site-lisp-dir load-path))))
-
 (require 'package)
 
-
-
-;;; Standard package repositories
-
-;;(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
-
-;; We include the org repository for completeness, but don't normally
-;; use it.
-(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
-
-(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
-
-(when (< emacs-major-version 24)
-  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
-
-;;; Also use Melpa for most packages
-(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/"))
+;;------------------------------------------------------------------------------
+;; Patch up annoying package.el quirks
+;;------------------------------------------------------------------------------
+(defadvice package-generate-autoloads (after close-autoloads (name pkg-dir) activate)
+  "Stop package.el from leaving open autoload files lying around."
+  (let ((path (expand-file-name (concat
+                                 ;; name is string when emacs <= 24.3.1,
+                                 (if (symbolp name) (symbol-name name) name)
+                                 "-autoloads.el") pkg-dir)))
+    (with-current-buffer (find-file-existing path)
+      (kill-buffer nil))))
 
 
-
-;; If gpg cannot be found, signature checking will fail, so we
-;; conditionally enable it according to whether gpg is available. We
-;; re-run this check once $PATH has been configured
-(defun sanityinc/package-maybe-enable-signatures ()
-  (setq package-check-signature (when (executable-find "gpg") 'allow-unsigned)))
+;;------------------------------------------------------------------------------
+;; Add support to package.el for pre-filtering available packages
+;;------------------------------------------------------------------------------
+(defvar package-filter-function nil
+  "Optional predicate function used to internally filter packages used by package.el.
 
-(sanityinc/package-maybe-enable-signatures)
-(eval-after-load 'init-exec-path
-  (sanityinc/package-maybe-enable-signatures))
+The function is called with the arguments PACKAGE VERSION ARCHIVE, where
+PACKAGE is a symbol, VERSION is a vector as produced by `version-to-list', and
+ARCHIVE is the string name of the package archive.")
 
+(defadvice package--add-to-archive-contents
+  (around filter-packages (package archive) activate)
+  "Add filtering of available packages using `package-filter-function', if non-nil."
+  (when (or (null package-filter-function)
+      (funcall package-filter-function
+         (car package)
+         (funcall (if (fboundp 'package-desc-version)
+          'package--ac-desc-version
+        'package-desc-vers)
+            (cdr package))
+         archive))
+    ad-do-it))
 
-
-;;; On-demand installation of packages
-
+;;------------------------------------------------------------------------------
+;; On-demand installation of packages
+;;------------------------------------------------------------------------------
 (defun require-package (package &optional min-version no-refresh)
-  "Install given PACKAGE, optionally requiring MIN-VERSION.
-If NO-REFRESH is non-nil, the available package lists will not be
-re-downloaded in order to locate PACKAGE."
+  "Ask elpa to install given PACKAGE."
   (if (package-installed-p package min-version)
       t
     (if (or (assoc package package-archive-contents) no-refresh)
@@ -57,47 +50,39 @@ re-downloaded in order to locate PACKAGE."
         (require-package package min-version t)))))
 
 
-(defun maybe-require-package (package &optional min-version no-refresh)
-  "Try to install PACKAGE, and return non-nil if successful.
-In the event of failure, return nil and print a warning message.
-Optionally require MIN-VERSION.  If NO-REFRESH is non-nil, the
-available package lists will not be re-downloaded in order to
-locate PACKAGE."
-  (condition-case err
-      (require-package package min-version no-refresh)
-    (error
-     (message "Couldn't install package `%s': %S" package err)
-     nil)))
+;;------------------------------------------------------------------------------
+;; Standard package repositories
+;;------------------------------------------------------------------------------
 
-
-;;; Fire up package.el
+;; We include the org repository for completeness, but don't use it.
+;; Lock org-mode temporarily:
+;; (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
 
-(setq package-enable-at-startup nil)
+(setq package-archives '(("melpa" . "http://melpa.org/packages/")
+                         ("melpa-stable" . "http://stable.melpa.org/packages/")
+                         ("gnu" . "http://elpa.gnu.org/packages/")))
+
+(defvar melpa-not-include-packages
+  '(yasnippet
+    swiper)
+  "Don't install these Melpa packages")
+
+;; Don't take Melpa versions of certain packages
+(setq package-filter-function
+      (lambda (package version archive)
+        (and
+         (not (memq package '(eieio)))
+         (or (and (string-equal archive "melpa") (not (memq package melpa-not-include-packages)))
+             (not (string-equal archive "melpa"))))))
+
+;; un-comment below code if you prefer use all the package on melpa (unstable) without limitation
+;; (setq package-filter-function nil)
+
+;;------------------------------------------------------------------------------
+;; Fire up package.el and ensure the following packages are installed.
+;;------------------------------------------------------------------------------
+
 (package-initialize)
-
-
-
-(require-package 'fullframe)
-(fullframe list-packages quit-window)
-
-
-(require-package 'cl-lib)
-(require 'cl-lib)
-
-(defun sanityinc/set-tabulated-list-column-width (col-name width)
-  "Set any column with name COL-NAME to the given WIDTH."
-  (cl-loop for column across tabulated-list-format
-           when (string= col-name (car column))
-           do (setf (elt column 1) width)))
-
-(defun sanityinc/maybe-widen-package-menu-columns ()
-  "Widen some columns of the package menu table to avoid truncation."
-  (when (boundp 'tabulated-list-format)
-    (sanityinc/set-tabulated-list-column-width "Version" 13)
-    (let ((longest-archive-name (apply 'max (mapcar 'length (mapcar 'car package-archives)))))
-      (sanityinc/set-tabulated-list-column-width "Archive" longest-archive-name))))
-
-(add-hook 'package-menu-mode-hook 'sanityinc/maybe-widen-package-menu-columns)
 
 
 (provide 'init-elpa)
