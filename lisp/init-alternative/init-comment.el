653fa29c36704d651e29e2d3c4343c84c1e8d837
ecnewbie <ecnewbie@gmail.com>
2016-08-25 21:38:44 +0800

    support lua multi line comment.

--- lisp/init-alternative/init-comment.el
+++ lisp/init-alternative/init-comment.el
@@ -9,11 +9,17 @@
     (c-mode
      ;; comment-start/end when region is active
      "/* " " */"
+     ;; comment-continue, not empty string
+     " * "
      ;; comment-start/end when region is not active
      "//" "")
     (c++-mode
      "/* " " */"
+     " * "
      "//" "")
+    (lua-mode
+     "--[[ " " ]]" "    "
+     "--" "")
     ))
 
 (defun smarter-call-comment-dwim-2 (&optional arg)
@@ -25,23 +31,27 @@
   "Comment with two style."
   (interactive "P")
   (let* ((old-comment-start comment-start)
-         (old-comment-end comment-end))
+         (old-comment-end comment-end)
+         (old-comment-continue comment-continue))
     (dolist (var smarter-comment-styles)
       (let* ((mode (car var))
              (region-comment-start (nth 1 var))
              (region-comment-end (nth 2 var))
-             (not-region-comment-start (nth 3 var))
-             (not-region-comment-end (nth 4 var)))
+             (region-comment-continue (nth 3 var))
+             (not-region-comment-start (nth 4 var))
+             (not-region-comment-end (nth 5 var)))
         (when (eq mode major-mode)
           (if (use-region-p)
               (progn
                 (setq-local comment-start region-comment-start)
-                (setq-local comment-end region-comment-end))
+                (setq-local comment-end region-comment-end)
+                (setq-local comment-continue region-comment-continue))
             (setq-local comment-start not-region-comment-start)
             (setq-local comment-end not-region-comment-end)))))
     (smarter-call-comment-dwim-2 arg)
     (setq-local comment-start old-comment-start)
-    (setq-local comment-end old-comment-end)))
+    (setq-local comment-end old-comment-end)
+    (setq-local comment-continue old-comment-continue)))
 
 ;; M-;
 (global-set-key [remap comment-dwim] 'smarter-comment-dwim)
