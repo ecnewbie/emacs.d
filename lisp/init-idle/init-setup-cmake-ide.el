0ab8decab8eaae5af70c02fb9891f4c61f185783
ecnewbie <ecnewbie@gmail.com>
2016-09-15 11:05:57 +0800

    speed up emacs.
    load rtags and cmake-ide later at mode-hook

--- lisp/init-idle/init-setup-cmake-ide.el
+++ lisp/init-idle/init-setup-cmake-ide.el
@@ -1,6 +1,15 @@
 (require 'init-cmake)
 
-(require 'rtags nil 'noerror)
-(cmake-ide-setup)
+(defun newbie/cmake-ide-setup()
+  (require 'cmake-ide nil 'noerror)
+  (when (featurep 'cmake-ide)
+    (require 'rtags nil 'noerror)
+    (cmake-ide-setup)))
+
+(add-hook 'c-mode-hook 'newbie/cmake-ide-setup)
+(add-hook 'c++-mode-hook 'newbie/cmake-ide-setup)
+
+(when (daemonp)
+  (newbie/cmake-ide-setup))
 
 (provide 'init-setup-cmake-ide)
