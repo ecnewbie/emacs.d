9cabda2d4100d462e42f15982c0321c3f24ad5c9
ecnewbie <ecnewbie@gmail.com>
2016-09-16 01:20:46 +0800

    speed up tramp.

--- lisp/init-idle/init-tramp.el
+++ lisp/init-idle/init-tramp.el
@@ -1,13 +1,14 @@
-(require 'tramp)
-
-(setq tramp-default-method "ssh")
-
-;; If you tramp is hanging, you can uncomment below line.
-;; (setq tramp-ssh-controlmaster-options "-o ControlMaster=auto -o ControlPath='tramp.%%C' -o ControlPersist=no")
+(with-eval-after-load 'tramp
+  ;; If you tramp is hanging, you can uncomment below line.
+  ;; (setq tramp-ssh-controlmaster-options "-o ControlMaster=auto -o ControlPath='tramp.%%C' -o ControlPersist=no")
+  (setq tramp-default-method "ssh"))
 
 (defun remote-eshell (username host)
   (interactive "sUsername: \nsHost: ")
   (let ((default-directory (format "/%s@%s:" username host)))
     (eshell host)))
+
+(when (daemonp)
+  (require 'tramp))
 
 (provide 'init-tramp)
