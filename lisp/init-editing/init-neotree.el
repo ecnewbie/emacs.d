ef36ae5f89f3fa7afd2665a886cdc77b4464c457
ecnewbie <ecnewbie@gmail.com>
2016-09-23 03:30:18 +0800

    fix neotree setup.

--- lisp/init-editing/init-neotree.el
+++ lisp/init-editing/init-neotree.el
@@ -3,7 +3,8 @@
 (require 'neotree)
 (defadvice delete-window (after kill-neotree-buffer activate)
   "Kill neotree buffer if it's window deleted."
-  (when (get-buffer (neo-global--get-buffer))
+  (when (and (neo-global--get-buffer)
+             (get-buffer (neo-global--get-buffer)))
     (kill-buffer (neo-global--get-buffer))))
 
 (provide 'init-neotree)
