69a0a59a3e453cf78db8aba77fe621588917d14b
ecnewbie <ecnewbie@gmail.com>
2016-09-22 21:15:08 +0800

    init-eval.el: require nothing.

--- lisp/init-prog/init-eval.el
+++ lisp/init-prog/init-eval.el
@@ -29,12 +29,10 @@
 
 ;;; cider support (for Clojure)
 (with-eval-after-load 'cider
-  (require 'eval-in-repl-cider)
   (define-key clojure-mode-map (kbd "<C-return>") 'eir-eval-in-cider))
 
 ;;; SLIME support (for Common Lisp)
 (with-eval-after-load 'slime
-  (require 'eval-in-repl-slime)
   (add-hook 'lisp-mode-hook
             '(lambda ()
                (local-set-key (kbd "<C-return>") 'eir-eval-in-slime))))
@@ -42,55 +40,45 @@
 ;;; Geiser support (for Racket and Guile Scheme)
 ;; When using this, turn off racket-mode and scheme supports
 (with-eval-after-load 'geiser
-  (require 'eval-in-repl-geiser)
   (add-hook 'geiser-mode-hook
             '(lambda ()
                (local-set-key (kbd "<C-return>") 'eir-eval-in-geiser))))
 
 ;;; Hy support
 (with-eval-after-load 'hy-mode
-  (require 'eval-in-repl-hy)
   (define-key hy-mode-map (kbd "<C-return>") 'eir-eval-in-hy))
 
 
 ;;; Python support
 (with-eval-after-load 'python
-  (require 'eval-in-repl-python)
   (add-hook 'python-mode-hook
             '(lambda ()
                (local-set-key (kbd "<C-return>") 'eir-eval-in-python))))
 
 ;;; Ruby support
 (with-eval-after-load 'ruby-mode
-  (require 'inf-ruby)
-  (require 'eval-in-repl-ruby)
   (define-key ruby-mode-map (kbd "<C-return>") 'eir-eval-in-ruby))
 
 ;;; SML support
 (with-eval-after-load 'sml-mode
-  (require 'eval-in-repl-sml)
   (define-key sml-mode-map (kbd "<C-return>") 'eir-eval-in-sml)
   (define-key sml-mode-map (kbd "C-;") 'eir-send-to-sml-semicolon))
 
 ;;; OCaml support
 (with-eval-after-load 'tuareg
-  (require 'eval-in-repl-ocaml)
   (define-key tuareg-mode-map (kbd "<C-return>") 'eir-eval-in-ocaml)
   ;; function to send a semicolon to OCaml REPL
   (define-key tuareg-mode-map (kbd "C-;") 'eir-send-to-ocaml-semicolon))
 
 ;;; Javascript support
 (with-eval-after-load 'js3-mode
-  (require 'eval-in-repl-javascript)
   (define-key js3-mode-map (kbd "<C-return>") 'eir-eval-in-javascript))
 (with-eval-after-load 'js2-mode
-  (require 'eval-in-repl-javascript)
   (define-key js2-mode-map (kbd "<C-return>") 'eir-eval-in-javascript))
 
 
 ;; Shell support
 (with-eval-after-load 'shell
-  (require 'eval-in-repl-shell)
   (add-hook 'sh-mode-hook
             '(lambda()
                (local-set-key (kbd "C-<return>") 'eir-eval-in-shell)))
