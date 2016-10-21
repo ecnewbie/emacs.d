76d65dda2d3b4a613dd1c637bc517bd89a14f6fb
ecnewbie <ecnewbie@gmail.com>
2016-10-21 22:09:43 +0800

    disable auto-insert.

--- lisp/init-editing/init-auto-insert.el
+++
@@ -1,7 +0,0 @@
-(auto-insert-mode 1)
-(setq auto-insert-query nil)
-(require-package 'yatemplate)
-;; templates with file name like "00:.*.cpp" in yatemplate-dir.
-(yatemplate-fill-alist)
-
-(provide 'init-auto-insert)
